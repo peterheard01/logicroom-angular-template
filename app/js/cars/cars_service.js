5e5d947c4f9cacda11927db3707062e6beb7c6c6
Peter Heard <peter_heard01@hotmail.com>
2015-06-17 02:19:57 +0100

    ready to refactor

--- app/js/cars/cars_service.js
+++ app/js/cars/cars_service.js
@@ -2,28 +2,28 @@ angular.module('app.services').service('CarsService',
     function($resource,CarsViewModel) {
 
         function load(){
+
                 var vehiclesResource =  $resource('',
                 {}, {GET: {url:'/api/vehicles.json', method: 'GET', isArray: true, cache: false}});
 
-            vehiclesResource.GET(function(vehicles){
+                vehiclesResource.GET(function(vehicles){
 
-                angular.forEach(vehicles,function(vehicle){
+                    angular.forEach(vehicles,function(vehicle){
 
-                    if(vehicle.type === 'car'){
+                        if(vehicle.type === 'car'){
 
-                        var car = {};
-                        car.name = vehicle.id;
-                        car.spec = vehicle.engineCap;
+                            var car = {};
+                            car.name = vehicle.id;
+                            car.spec = vehicle.engineCap;
 
-                        CarsViewModel.Cars.push(car);
-                    }
+                            CarsViewModel.Cars.push(car);
+                        }
 
 
-                });
+                    });
 
             });
         }
-
         return{
             Load : load
         };
